0fd0ba42f81f103181bba614a75a9bea47394365
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-03-10 11:21:48 -0600

    Basic page

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function MainContent() {
+    return(
+        <h1>This is where the Main Content goes!</h1>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <h1>This is where the Navbar goes!</h1>
+    )
+}
