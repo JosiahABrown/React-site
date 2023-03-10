ac9dbfe77442b2b82dd06b457bcaeda5854a68c8
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-03-10 12:36:20 -0600

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react"
+import logo from "../images/reactjs-logo-small.png"
 
 export default function Navbar() {
     return (
-        <h1>This is where the Navbar goes!</h1>
+        <nav className="nav">
+            <img className="nav-logo" src={logo} />
+            <h3 className="nav-title nav-title-main">ReactFacts</h3>
+            <h4 className="nav-title nav-title-sub">React Course | Project 1</h4>
+        </nav>
     )
 }
